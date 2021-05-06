5ded903c14a2c8190b61d1f759b56fab31d55cc1
El Roma <r.huc@orange.fr>
2021-05-06 14:53:02 +0200

    fix git

--- backend/puppeteer.js
+++ backend/puppeteer.js
@@ -5,6 +5,5 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     await page.goto('https://example.com');
     await page.screenshot({ path: 'example.png' });
-//test
     await browser.close();
 })();
