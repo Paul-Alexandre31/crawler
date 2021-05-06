e60e4e55d562c2a89a2148b48bd4281d625abd6d
Paul-Alexandre31 <fourcadepaulalexandre@gmail.com>
2021-05-06 14:52:36 +0100

    add url stockx

--- backend/puppeteer.js
+++ backend/puppeteer.js
@@ -3,8 +3,9 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
-    await page.goto('https://example.com');
+    await page.goto('https://stockx.com/fr-fr/sneakers/most-popular');
     await page.screenshot({ path: 'example.png' });
+    dsfdsf
 //test
-    await browser.close();
+   // await browser.close();
 })();
