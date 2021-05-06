66f2ea4fc414c319578094902f3699a050c3c532
El Roma <r.huc@orange.fr>
2021-05-06 17:26:45 +0200

    add name & nbr sale

--- backend/puppeteer.js
+++ backend/puppeteer.js
@@ -11,7 +11,8 @@ const puppeteer = require('puppeteer');
                 url: tile.querySelector('a').href,
                 price: parseInt(tile.querySelector('.price-line-div div:nth-child(2)').innerText),
                //photo: tile.querySelector('img').src, #recup img quand html fonctionel
-
+                name: tile.querySelector('a div:nth-child(2) div').innerText,
+                nbrSell : tile.querySelector('a div:nth-child(2) div:nth-child(3)').innerText.slice(14)
             })
         });
         return products
