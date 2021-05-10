c9a1588296b0f11145cb4aa7e8f31f240a305769
El Roma <r.huc@orange.fr>
2021-05-10 17:21:24 +0200

    card fix

--- backend/puppeteer.js
+++ backend/puppeteer.js
@@ -10,7 +10,7 @@ module.exports = async function(){
             products.push({
                 url: tile.querySelector('a').href,
                 price: parseInt(tile.querySelector('.price-line-div div:nth-child(2)').innerText),
-                //photo: tile.querySelector('img').src, //#recup img quand html fonctionel
+                photo: tile.querySelector('img').src, //#recup img quand html fonctionel
                 name: tile.querySelector('a div:nth-child(2) div').innerText,
                 nbrSell : tile.querySelector('a div:nth-child(2) div:nth-child(3)').innerText.slice(14)
             })
