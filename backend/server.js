692919b6a3a83c9d7d0ce75eff2067764385be8d
Paul-Alexandre31 <fourcadepaulalexandre@gmail.com>
2021-05-12 14:40:33 +0100

    add card

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ const scanner = require('./puppeteer');
 
    app.set('view engine', 'ejs');
    app.set('views', 'pages/');
-
    app.use(express.static('public'));
 
    app.get('/', function (req, res) {
@@ -16,6 +15,12 @@ const scanner = require('./puppeteer');
          list: list,
       });
    });
+
+   app.get('/main.css', ((req, res) => {
+      res.sendFile("C:\\Users\\fourc\\OneDrive\\Bureau\\crawler\\backend\\public\\css\\main.css", {
+         type: 'text/css'
+      })
+   }))
 
    app.listen(3000, ()=>{
       console.log('Ca roule sur le port :3000')
