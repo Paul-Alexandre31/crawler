c01374c39174775e64746a8c8e7a276a0f56b64d
Corentin Baudrin <corentin.baudrin@ynov.com>
2021-05-12 13:51:36 +0200

    Patch photo

--- backend/puppeteer.js
+++ backend/puppeteer.js
@@ -1,12 +1,22 @@
 const puppeteer = require('puppeteer');
 
 module.exports = async function(){
+
+
+
     const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
     await page.goto('https://stockx.com/fr-fr/sneakers/most-popular');
-    let list = await page.$$eval('[data-testid="product-tile"]', (tiles)=> {
+    let list = await page.$$eval('[data-testid="product-tile"]', async (tiles)=> {
         let products = [];
-        tiles.forEach((tile)=>{
+
+        function delay(s){
+            return new Promise((resolve)=>setTimeout(resolve, s));
+        }
+
+        for (const tile of tiles) {
+            tile.scrollIntoView();
+            await delay(10);
             products.push({
                 url: tile.querySelector('a').href,
                 price: parseInt(tile.querySelector('.price-line-div div:nth-child(2)').innerText),
@@ -14,7 +24,8 @@ module.exports = async function(){
                 name: tile.querySelector('a div:nth-child(2) div').innerText,
                 nbrSell : tile.querySelector('a div:nth-child(2) div:nth-child(3)').innerText.slice(14)
             })
-        });
+        }
+
         return products
     });
 
